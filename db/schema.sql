9beeaa013d6c3df7a47825cc932c329e82ac5573
shaden-pr <pratiamalden@gmail.com>
2025-10-08 21:42:48 +0100

    Add database character limits for blooms

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
 CREATE TABLE blooms (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     sender_id INT NOT NULL REFERENCES users(id),
-    content TEXT NOT NULL,
+    content TEXT NOT NULL CHECK (CHAR_LENGTH(content) <= 280),
     send_timestamp TIMESTAMP NOT NULL
 );
 
